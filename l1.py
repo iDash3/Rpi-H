720f9ac63d9a5f5e0b03d0a2e84e632694cabe04
iDash3 <idash3.b@gmail.com>
2017-11-10 21:18:09 -0600

    Style added.

--- l1.py
+++ l1.py
@@ -6,13 +6,17 @@ GPIO.setup(11, GPIO.OUT)
 GPIO.setwarnings(False)
 
 class Lights(object):
-        def __init__(self):
-                pass
+    def __init__(self):
+        pass
 
-        def lights_on(self):
-                GPIO.output(11, GPIO.HIGH)
-                print('Lights on')
+    def lights_on(self):
+        GPIO.output(11, GPIO.HIGH)
 
-        def lights_off(self):
-                GPIO.output(11, GPIO.LOW)
-                print('Lights off')
+    def lights_off(self):
+		GPIO.output(11, GPIO.LOW)
+
+    def show_off(self):
+		GPIO.output(11, GPIO.HIGH)        	
+		time.sleep(1)
+		GPIO.output(11, GPIO.LOW)
+		time.sleep(1)
